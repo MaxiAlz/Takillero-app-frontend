3cbb392c66cdeac09cbb037bf0199773f7c10763
Maxi Alzugaray <maxialz99@hotmail.com>
2024-10-12 13:40:21 -0300

    instalo y configuro redux para sesion de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Loader from './common/Loader';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkUserSession } from './redux/slices/auth/authThunk';
+import { AppDispatch, RootState } from './redux/store';
 import RouteIndex from './routes/RouterIndex';
+import Loader from './common/Loader';
+import { AuthStatus } from './modules/Auth/types/authTypes';
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
+  // const [loading, setLoading] = useState<boolean>(true);
   const { pathname } = useLocation();
+  const dispatch: AppDispatch = useDispatch();
+  const { status } = useSelector((state: RootState) => state.auth);
+
+  const verifySession = async () => {
+    dispatch(checkUserSession());
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+    verifySession();
+  }, [dispatch]);
 
-  return loading ? <Loader /> : <RouteIndex />;
+  return status === AuthStatus.CHECKING ? <Loader /> : <RouteIndex />;
 }
 
 export default App;
---
+++ src/redux/slices/auth/authThunk.ts
@@ -0,0 +1,43 @@
+import { Action, Dispatch, ThunkAction } from '@reduxjs/toolkit';
+import { RootState } from '../../store';
+import { apiService } from '../../../api/apiService';
+import { logoutUser, setError, setUserAuthenticated } from './authSlice';
+import {
+  UserLoginData,
+  // UserProfile,
+} from '../../../modules/Auth/types/authTypes';
+
+const loginUser =
+  (
+    user: UserLoginData,
+  ): ThunkAction<void, RootState, unknown, Action<string>> =>
+  async (dispatch: Dispatch) => {
+    try {
+      const response = await apiService.post('/Auth/login', user);
+      console.log('response', response);
+      if (response.status == 200) {
+        dispatch(setUserAuthenticated(response.data));
+        return response;
+      }
+    } catch (error) {
+      dispatch(setError(`Failed to login ${error}`));
+      dispatch(logoutUser());
+    }
+  };
+
+const checkUserSession =
+  (): ThunkAction<void, RootState, unknown, Action<string>> =>
+  async (dispatch: Dispatch) => {
+    try {
+      const response = await apiService.get('/Auth/profile');
+      if (response.status === 200) {
+        dispatch(setUserAuthenticated(response.data)); // Almacena los datos del usuario en Redux
+      } else {
+        dispatch(logoutUser()); // Si la cookie es inválida, cierra la sesión
+      }
+    } catch (error) {
+      dispatch(logoutUser());
+    }
+  };
+
+export { loginUser, checkUserSession };
