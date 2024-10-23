91d13c4a5f6e69184631d3a2859307dbc6fb1334
Maxi Alzugaray <maxialz99@hotmail.com>
2024-10-23 19:38:18 -0300

    modifico componetnes

--- src/constants/panel/dropdownUser_items-.ts
+++ src/constants/panel/dropdownUser_items-.ts
@@ -5,14 +5,15 @@ import {
 } from 'react-icons/md';
 
 const dropdown_user_links = [
-  { itemName: 'Mi perfil', icon: MdPersonOutline, link: '/profile' },
+  { itemName: 'Mi perfil', icon: MdPersonOutline, link: '/user/profile' },
   { itemName: 'Mi Panel', icon: MdOutlineDashboard, link: '/panel' },
+
   {
     itemName: 'Configuraciones',
     icon: MdOutlineSettings,
     link: '/panel/settings',
   },
-  // { item: 'Cerrar sesion', icon: 'MdLogout  ' },
+
 ];
 
 export { dropdown_user_links };
